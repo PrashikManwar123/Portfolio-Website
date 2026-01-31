dfc2964bfc30fe873090deb96ef5680150e21cac
prashikmanwar911 <167643811+PrashikManwar123@users.noreply.github.com>
2026-01-31 09:53:01 +0530

    update index.html and css

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,31 @@
   "use strict";
 
     // COLOR MODE
-    $('.color-mode').click(function(){
-        $('.color-mode-icon').toggleClass('active')
-        $('body').toggleClass('dark-mode')
-    })
+    function setDarkMode(on) {
+        if (on) {
+            $('#colorToggle').addClass('active').attr('aria-pressed', 'true');
+            $('body').addClass('dark-mode');
+        } else {
+            $('#colorToggle').removeClass('active').attr('aria-pressed', 'false');
+            $('body').removeClass('dark-mode');
+        }
+    }
+
+    // Click handler for toggle
+    $(document).on('click', '#colorToggle', function(){
+        var next = !$('body').hasClass('dark-mode');
+        setDarkMode(next);
+        try { localStorage.setItem('darkMode', next ? 'on' : 'off'); } catch (e) {}
+    });
+
+    // Initialize from localStorage
+    $(function() {
+        try {
+            var stored = localStorage.getItem('darkMode');
+            if (stored === 'on') setDarkMode(true);
+            else if (stored === 'off') setDarkMode(false);
+        } catch (e) {}
+    });
 
     // HEADER
     $(".navbar").headroom();
